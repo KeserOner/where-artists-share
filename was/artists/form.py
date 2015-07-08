2d11fd76a4c60a049e2ce033f79f3ca55441f543
KeserOner <jbaptiste.jaillet@gmail.com>
2015-07-08 18:33:42 +0200

    Change forms for user to have a cleaner thing

--- was/artists/form.py
+++ was/artists/form.py
@@ -1,39 +1,19 @@
 #-*- coding: utf-8 -*-
 
-from django import forms
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
+class CreateArtistForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('username', 'email')
 
-class RegisterArtistForm(forms.Form):
-    email = forms.EmailField(label='Adresse mail', required=True)
-    username = forms.CharField(label='Nom utilisateur', required=True)
-    password = forms.CharField(label='Mot de passe', min_length=6, required=True, widget=forms.PasswordInput)
-    password_confirm = forms.CharField(label='Confirmation du mot de passe',
-                                       min_length=6,
-                                       required=True,
-                                       widget=forms.PasswordInput)
+    def save(self, commit=True):
+        user = super(CreateArtistForm, self).save(commit=False)
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+        return user
 
 
 
-    def clean(self):
-        cleaned_data = super(RegisterArtistForm, self).clean()
-
-        if 'password' in cleaned_data and 'password_confirm' in cleaned_data:
-            if cleaned_data['password'] != cleaned_data['password_confirm']:
-                msg = 'Les mots de passe sont différents'
-                self.errors['password'] = self.error_class([msg])
-                self.errors['password_confirm'] = self.error_class([msg])
-
-            del cleaned_data['password']
-            del cleaned_data['password_confirm']
-
-        username = cleaned_data.get('username')
-        self.validateUsername(username)
-
-        email = cleaned_data.get('email')
-        self.validateEmail(email)
-
-        return cleaned_data
-
-    def throwError(self, key=None, message=None):
-        self.errors[key] = self.error_class[(message)]
--- was/artists/models.py
+++ was/artists/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
 
 # Create your models here.
 
@@ -8,7 +9,8 @@ class Artists(models.Model):
     user = models.OneToOneField(User)
     artist_image = models.ImageField(null=True, blank=True, upload_to="/artist_image/")
 
-
     def __str__(self):
         return 'Profil de {0}'.format(self.username)
 
+    def get_absolute_url(self):
+        return reverse('artist-details', kwargs={'pk': self.pk})
