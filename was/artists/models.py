a415958564aec4c491432e134e76b47c72269806
KeserOner <jbaptiste.jaillet@gmail.com>
2015-07-16 16:33:43 +0200

    Add fields to artist and change __str__ method

--- was/artists/models.py
+++ was/artists/models.py
@@ -8,9 +8,9 @@ from django.core.urlresolvers import reverse
 class Artists(models.Model):
     user = models.OneToOneField(User)
     artist_image = models.ImageField(null=True, blank=True, upload_to="/artist_image/")
+    artist_banner = models.ImageField(null=True, blank=True, upload_to="/artist_image")
+    artist_bio = models.TextField(max_length=500)
+    artist_signature = models.CharField(max_length=70)
 
     def __str__(self):
-        return 'Profil de {0}'.format(self.username)
-
-    def get_absolute_url(self):
-        return reverse('artist-details', kwargs={'pk': self.pk})
+        return 'Profil de {0}'.format(self.user.username)
