515dbdc7ba9da28629c31618fea59adf48060f0e
KeserOner <jbaptiste.jaillet@gmail.com>
2015-07-17 14:18:51 +0200

    add new url and re format model.

--- was/artists/models.py
+++ was/artists/models.py
@@ -1,14 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.core.urlresolvers import reverse
-
-# Create your models here.
 
 
 class Artists(models.Model):
     user = models.OneToOneField(User)
-    artist_image = models.ImageField(null=True, blank=True, upload_to="/artist_image/")
-    artist_banner = models.ImageField(null=True, blank=True, upload_to="/artist_image")
+    artist_image = models.ImageField(null=True, blank=True, upload_to="artist_image/")
+    artist_banner = models.ImageField(null=True, blank=True, upload_to="artist_image/")
     artist_bio = models.TextField(max_length=500)
     artist_signature = models.CharField(max_length=70)
 
--- was/artists/urls.py
+++ was/artists/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     url(r'^register/?$', CreateArtistView.as_view(), name='register_user'),
     url(r'^login/?$', artist_login, name='artist_login'),
     url(r'^logout/?$', artist_logout, name='artist_logout'),
-    url(r'^update/?$', UpdateArtistView.as_view(), name='update_user')
-]
+    url(r'^update/?$', UpdateArtistView.as_view(), name='update_artist'),
+    ]
