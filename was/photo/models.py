559f3c18a7e27e4bb1147b03a27ec083a66749d0
KeserOner <jbaptiste.jaillet@gmail.com>
2015-07-27 01:56:34 +0200

    Define a '__str__' method for photo model

--- was/photo/models.py
+++ was/photo/models.py
@@ -5,3 +5,6 @@ class Photo(models.Model):
     artist = models.ForeignKey('artists.Artists')
     picture = models.ImageField(null=True, blank=True, upload_to="art_picture/")
     comment = models.TextField(max_length=500)
+
+    def __str__(self):
+        return '{}'.format(self.picture)
