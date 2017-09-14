9440a59c0e9519a5184ea46997eef5722dfde525
KeserOner <jbaptiste.jaillet@gmail.com>
2017-09-14 21:30:13 +0200

    add album listview with minimal template

--- was/photo/urls.py
+++ was/photo/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
-from .views import upload_photo_artist, delete_photo_artist
+from .views import (upload_photo_artist, delete_photo_artist,
+                    AlbumListView)
 
 
 urlpatterns = [
     url(r'^upload/?$', upload_photo_artist, name='upload_photo_artist'),
-    url(r'^(?P<photo_id>\d+)/delete/$', delete_photo_artist, name='delete_photo_artist')
+    url(r'^(?P<photo_id>\d+)/delete/$', delete_photo_artist, name='delete_photo_artist'),
+    url(r'^album/(?P<user_pk>\d+)/$', AlbumListView.as_view(), name='list_artist_albums')
     ]
--- was/photo/views.py
+++ was/photo/views.py
@@ -1,8 +1,10 @@
 from .forms import UploadPhotoForm
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.views.decorators.http import require_POST
 from django.contrib.auth.decorators import login_required
-from .models import Photo
+from django.views.generic import ListView
+from artists.models import Artists
+from .models import Photo, Album, AlbumPhotoRelation
 import json
 
 
@@ -33,3 +35,25 @@ def upload_photo_artist(request):
 def delete_photo_artist(request, photo_id):
     photo = Photo.objects.get(id=photo_id)
     photo.delete()
+
+
+class AlbumListView(ListView):
+    model = Album
+    template_name = 'album_list.html'
+    context_object_name = 'albums'
+
+    def get_queryset(self):
+        user_pk = self.kwargs.get('user_pk', '')
+        if not user_pk:
+            return Http404()
+
+        artist = Artists.objects.get(user__pk=user_pk)
+        queryset = {}
+        for album in Album.objects.filter(artist__pk=artist.pk):
+            try:
+                photo = AlbumPhotoRelation.objects.filter(album=album)[0].photo
+            except IndexError:
+                photo = ''
+            queryset[album.title] = (photo, album.pk)
+
+        return queryset
