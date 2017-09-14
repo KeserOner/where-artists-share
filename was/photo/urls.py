b149e2089ee819f59e920f8c295c623dce813ab7
KeserOner <jbaptiste.jaillet@gmail.com>
2017-09-14 22:47:07 +0200

    Add login requirement on create album view

--- was/photo/urls.py
+++ was/photo/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.contrib.auth.decorators import login_required
 from .views import (upload_photo_artist, delete_photo_artist,
                     AlbumListView, CreateAlbumView)
 
@@ -7,5 +8,5 @@ urlpatterns = [
     url(r'^upload/?$', upload_photo_artist, name='upload_photo_artist'),
     url(r'^(?P<photo_id>\d+)/delete/$', delete_photo_artist, name='delete_photo_artist'),
     url(r'^album/(?P<user_pk>\d+)/$', AlbumListView.as_view(), name='list_artist_albums'),
-    url(r'^create-album/$', CreateAlbumView.as_view(), name='create_album')
+    url(r'^create-album/$', login_required(CreateAlbumView.as_view()), name='create_album')
 ]
