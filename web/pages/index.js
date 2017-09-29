902bc128056d684855112c9593286883d77826e2
Erazihel <thomas.lepore@gmail.com>
2017-09-29 22:29:02 +0200

    Add flow

--- web/pages/index.js
+++ web/pages/index.js
@@ -1,1 +1,11 @@
-export default () => <div>Welcome to next.js!</div>
+// @flow
+
+import React from 'react'
+
+class App extends React.Component<any, any> {
+  render() {
+    return <div>Welcome to next.js!</div>
+  }
+}
+
+export default App
