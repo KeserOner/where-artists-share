923dc48b054411a08938da400f53920a647bfb5f
Erazihel <thomas.lepore@gmail.com>
2017-10-19 00:01:24 +0200

    Add ESLint

--- web/src/App/index.js
+++ web/src/App/index.js
@@ -1,8 +1,7 @@
 // @flow
 
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import NavbarComponent from '../Navbar';
 
